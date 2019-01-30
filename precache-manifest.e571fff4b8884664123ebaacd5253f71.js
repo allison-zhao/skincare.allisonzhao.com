67bcf4a07785a302e71fb20354c30aef2ad3db4c
Allison von Bargen <gryffinzhao@gmail.com>
2019-01-30 18:36:47 -0500

    Updates

---
+++ precache-manifest.e571fff4b8884664123ebaacd5253f71.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e6c672e8cf4b4e2c0f1",
+    "url": "/static/js/main.6e6c672e.chunk.js"
+  },
+  {
+    "revision": "d483c4153cde72fce481",
+    "url": "/static/js/1.d483c415.chunk.js"
+  },
+  {
+    "revision": "d6956324aad3db6d0798f55d8fe463a5",
+    "url": "/index.html"
+  }
+];
